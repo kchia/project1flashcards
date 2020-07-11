bb4f66176fc9b3f985c7c517d6ccfafaef8e2aae
alexpowers429 <alex.powers429@gmail.com>
2020-07-10 19:42:07 -0500

    updated my JS with some skeleton code with pseudocode written in for what i want JS to do

---
+++ app.js
@@ -0,0 +1,12 @@
+Js console.log('hello there')
+// gets a question
+function getAQuestion () {}
+
+// shows hint
+function getAHint(){}
+
+// shows other peoples answers
+function seeCommonAnswers () {}
+
+
+function getAnother () {}
