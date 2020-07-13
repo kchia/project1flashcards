938ffdf0b22efce085104fb75db53c70651f2615
alexpowers429 <alex.powers429@gmail.com>
2020-07-13 13:18:12 -0500

    added a function into JS with the help of Carlos to make the card flip animation smoother. I also added in an array of questions and answers

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
-Js console.log('hello there')
-// gets a question
-function getAQuestion () {}
+// function flipCard() {
+// 	document.querySelector('.card').classList.toggle('flipped');
+// }
+// document.querySelector('.card').addEventListener('click', flipCard);
+
+let questionAnswer = [
+     tljDirector ={ question: 'Who directed The Last Jedi?',
+      answer: 'Rian Johnson',
+    },
+     AnakinPada ={
+        question: 'Who was the Padawan of Anakin Skywalker?',
+        answer: 'Ashsoka Tano',
+    },
+    planetName = {
+        question: 'What planet was Han Solo born on?',
+        answer: 'Corelia',
+    },
+     yodaSpecies = {
+        question: 'What species does Master Yoda belong to?',
+        answer: 'George Lucas and Disney have not decided yet!',
+    },
+     lukeName = {
+        question: 'What was the last name of Luke Skywalker in the original draft of A New Hope?',
+        answer: 'Starkiller',
+    },
+     obiEnemy ={
+        question: 'Who was the main adversary of Obi-Wan-Kenobi throughout the majority of his life?',
+        answer: 'Darth-Maul',
+    },
+    rebelShip ={
+        question: 'What kind of ship did the Rebel hero, Luke Skywalker fly when he destroyed the first death-star?',
+        answer: 'X-Wing',
+    },
+     iAmYourFather = {
+        question: 'What was written in the script to prevent leaks for the big twist at the end of Empire Strikes back?',
+        answer: 'Obi-Wan killed your father',
+    },
+    benSwolo = {
+        question: 'What was the birth name of Kylo Ren?',
+        answer: 'Ben Solo',
+    },
+     theTragedy = {
+        question: 'Who was the Dark Lord that trained Emperor Palpatine?',
+        answer: 'Darth Plagueis The Wise',
+    }
+]
+console.log(questionAnswer)
+
+function getAQuestion (event) {
+
+}
 
 // shows hint
 function getAHint(){}
 
+//shows answer
+
+function getAnAnswer() {}
+
 // shows other peoples answers
 function seeCommonAnswers () {}
 
+function getANewQuestion () {}
 
-function getAnother () {}
+
+function restartGame (){}
